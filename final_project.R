86bdf50baf16e4c0e86afad2b943050af79a7fb4
dsung4 <dsung@uw.edu>
2023-03-07 16:01:41 -0800

    ....

--- final_project.R
+++ final_project.R
@@ -2,9 +2,7 @@ library(shiny)
 library(tidyverse)
 library(rsconnect)
 library(tidyr)
-##hi
-#hola
-## konichiwa
+
 totalStats <- read_delim("Count Us In/Total-Table 1.csv")
 
 demographics <- read_delim("Count Us In/Demographics-Table 1.csv")
